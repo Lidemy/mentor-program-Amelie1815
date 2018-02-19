9653e2771b7b465e27da850552fb4e169dca8dff
Amelie Cheng <amelie1815@gmail.com>
2018-02-19 18:37:29 +0000

    訂正w1hw1

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,6 +1,6 @@
 export const stars = (n) => {
   let arr = [];
-  for (i = 1; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {    // 本來 i 忘記宣告，已訂正
       arr.push('*'.repeat(i));    // 一開始用 '*' * i 失敗，因為 '*' 不是數字無法使用乘法
   }
   return arr;
